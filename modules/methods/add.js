8086277aec40c0db03e21b1e596deb25e7a5de42
Matt Downs <mattdowns6@gmail.com>
2018-01-15 20:52:45 +1000

    Moved postApi() call to add and update classes

--- modules/methods/add.js
+++ modules/methods/add.js
@@ -1,3 +1,6 @@
+const sharedModule = require('../../shared');
+
+
 class AddModule {
     constructor(data) {
         this.data = [];
@@ -12,6 +15,15 @@ class AddModule {
         } else this.data.push(data);
 
         return this;
+    }
+
+    exec({ debug = false, action, schema }) {
+        let body = {};
+        body[schema] = this.data;
+        let req = { action: action, body: body };
+
+        if (debug) return req;
+        return sharedModule.postApi(req);
     }
 }
 
--- modules/methods/update.js
+++ modules/methods/update.js
@@ -1,3 +1,6 @@
+const sharedModule = require('../../shared');
+
+
 class UpdateModule {
     constructor(data) {
         this.data = [];
@@ -12,6 +15,15 @@ class UpdateModule {
         } else this.data.push(data);
 
         return this;
+    }
+
+    exec({ debug = false, action, schema }) {
+        let body = {};
+        body[schema] = this.data;
+        let req = { action: action, body: body };
+
+        if (debug) return req;
+        return sharedModule.postApi(req);
     }
 }
 
