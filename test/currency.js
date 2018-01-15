d051ed120b047baabc43a1df8e9ef1b4f6993531
Matt Downs <mattdowns6@gmail.com>
2018-01-15 21:22:28 +1000

    100% test coverage on currency module!

--- test/currency.js
+++ test/currency.js
@@ -3,12 +3,86 @@ const Joi = require('Joi');
 
 describe('currency', function() {
 
-    it('should contain a .getSettings() function', function() {
-        Joi.assert(this.api.currency.getSettings, Joi.func().required());
+    describe('.getSettings()', function() {
+
+        it('should be a function', function() {
+            Joi.assert(this.api.currency.getSettings, Joi.func().required());
+        });
+
+        describe('.exec()', function() {
+
+            it('should be a function', function() {
+                Joi.assert(this.api.currency.getSettings().exec, Joi.func().required());
+            });
+
+            it('should return a promise', function() {
+                Joi.assert(this.api.currency.getSettings().exec().then, Joi.func()
+                    .required());
+            });
+
+        })
+
+        describe('request', function() {
+
+            before(function() {
+                this.data = this.api.currency
+                    .getSettings()
+                    .exec({ debug: true });
+            });
+
+            it('should contain the correct API action', function() {
+                let schema = Joi.string().valid('GetCurrencySettings').required();
+                Joi.assert(this.data.action, schema);
+            });
+
+        });
+        
     });
 
-    it('should contain a .updateSettings() function', function() {
-        Joi.assert(this.api.currency.updateSettings, Joi.func().required());
+
+    describe('.updateSettings()', function() {
+
+        it('should be a function', function() {
+            Joi.assert(this.api.currency.updateSettings, Joi.func().required());
+        });
+        
+        describe('.exec()', function() {
+
+            it('should be a function', function() {
+                Joi.assert(this.api.currency.updateSettings().exec, Joi.func().required());
+            });
+
+            it('should return a promise', function() {
+                Joi.assert(this.api.currency.updateSettings().exec().then, Joi.func()
+                    .required());
+            });
+
+        })
+
+        describe('request', function() {
+
+            before(function() {
+                this.data = this.api.currency
+                    .updateSettings({ DEFAULTCOUNTRY: 'test' })
+                    .exec({ debug: true });
+            });
+
+            it('should contain the correct API action', function() {
+                let schema = Joi.string().valid('UpdateCurrencySettings').required();
+                Joi.assert(this.data.action, schema);
+            });
+
+            it('should fit the correct body schema', function() {
+                let schema = {
+                    CurrencySettings: {
+                        DEFAULTCOUNTRY: Joi.string().required()
+                    }
+                };
+                Joi.assert(this.data.body, schema);
+            });
+
+        });
+
     });
 
 });
