cc617806bc59652bcf4ab87eef74689cd234af3d
Matt Downs <mattdowns6@gmail.com>
2019-06-09 09:54:40 +1000

    Remove unused setup function from test

--- __tests__/initialiser.test.js
+++ __tests__/initialiser.test.js
@@ -1,34 +1,22 @@
-const { NetoAPI } = require('../dist/index');
-const assert = require('assert');
+const { NetoAPI } = require("../dist/index");
+const assert = require("assert");
 
+describe("initialiser", function() {
+  it("should throw exception if key is missing from config", function() {
+    const myFunction = () => {
+      new NetoAPI({
+        url: "https://someurl.com"
+      });
+    };
+    assert.throws(myFunction, /key must be specified/);
+  });
 
-module.exports.setup = () => {
-    return new NetoAPI({
-        url: 'https://someurl.com',
-        user: 'someuser',
-        key: 'somekey'
-    });
-}
-
-
-describe('initialiser', function() {
-
-    it('should throw exception if key is missing from config', function() {
-        const myFunction = () => {
-            new NetoAPI({
-                url: 'https://someurl.com'
-            });
-        };
-        assert.throws(myFunction, /key must be specified/);
-    });
-
-    it('should throw exception if url is missing from config', function() {
-        const myFunction = () => {
-            new NetoAPI({
-                key: 'somekey'
-            });
-        };
-        assert.throws(myFunction, /url must be specified/);
-    });
-    
+  it("should throw exception if url is missing from config", function() {
+    const myFunction = () => {
+      new NetoAPI({
+        key: "somekey"
+      });
+    };
+    assert.throws(myFunction, /url must be specified/);
+  });
 });
