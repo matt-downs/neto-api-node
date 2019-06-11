234293ae7614ba117b5c6f2ba7efe8475c948a9d
Matt Downs <matt.downs@neto.com.au>
2019-06-11 11:15:13 +1000

    Update initialiser tests

--- __tests__/initialiser.test.js
+++ __tests__/initialiser.test.js
@@ -1,22 +1,94 @@
 const { NetoAPI } = require("../dist/index");
-const assert = require("assert");
 
-describe("initialiser", function() {
-  it("should throw exception if key is missing from config", function() {
+describe("initialiser", () => {
+  it("should throw exception if key, oauth_clientId and oauth_secret are missing from config", () => {
+    // Arrange
     const myFunction = () => {
       new NetoAPI({
         url: "https://someurl.com"
       });
     };
-    assert.throws(myFunction, /key must be specified/);
+
+    // Act
+
+    // Assert
+    expect(myFunction).toThrowError(
+      /^key or oauth_clientId \+ oauth_secret must be specified$/
+    );
   });
 
-  it("should throw exception if url is missing from config", function() {
+  it("should throw exception if oauth_secret is missing from config", () => {
+    // Arrange
+    const myFunction = () => {
+      new NetoAPI({
+        url: "https://someurl.com",
+        oauth_clientId: "someclientid"
+      });
+    };
+
+    // Act
+
+    // Assert
+    expect(myFunction).toThrowError(
+      /^key or oauth_clientId \+ oauth_secret must be specified$/
+    );
+  });
+
+  it("should throw exception if oauth_clientId is missing from config", () => {
+    // Arrange
+    const myFunction = () => {
+      new NetoAPI({
+        url: "https://someurl.com",
+        oauth_secret: "somesecret"
+      });
+    };
+
+    // Act
+
+    // Assert
+    expect(myFunction).toThrowError(
+      /^key or oauth_clientId \+ oauth_secret must be specified$/
+    );
+  });
+
+  it("should throw exception if url is missing from config", () => {
+    // Arrange
     const myFunction = () => {
       new NetoAPI({
         key: "somekey"
       });
     };
-    assert.throws(myFunction, /url must be specified/);
+
+    // Act
+
+    // Assert
+    expect(myFunction).toThrowError(/^url must be specified$/);
+  });
+
+  it("should initialise properly with url and key", () => {
+    // Arrange
+    const mySite = new NetoAPI({
+      url: "https://someurl.com",
+      key: "somekey"
+    });
+
+    // Act
+
+    // Assert
+    expect(mySite).toBeInstanceOf(NetoAPI);
+  });
+
+  it("should initialise properly with url, oauth_clientId and oauth_secret", () => {
+    // Arrange
+    const mySite = new NetoAPI({
+      url: "https://someurl.com",
+      oauth_clientId: "someclientid",
+      oauth_secret: "somesecret"
+    });
+
+    // Act
+
+    // Assert
+    expect(mySite).toBeInstanceOf(NetoAPI);
   });
 });
